2876cedeba3f53b3b26162ba1b38f7d5a9d02c0b
yaser <ydahgan@gmail.com>
2024-02-09 17:13:12 +0330

    use email to send secrert code

--- Accounts/views.py
+++ Accounts/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import login as dj_login
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.core.mail import send_mail
 from .forms import MyUserCreationForm, MyUserChangeForm, SecertCodeForm
 from .secret import secert_code_genarator, encode_int_urlsafe, decode_int_urlsafe
 
@@ -21,43 +22,69 @@ def login(request):
             dj_login(request, user)
             return redirect("visit_profile")
         else:
-            return render(request, "Accounts/templates/registration/login.html", context={"form": user_form})
+            return render(
+                request,
+                "Accounts/templates/registration/login.html",
+                context={"form": user_form},
+            )
     else:
         login_form = AuthenticationForm()
-        return render(request, "Accounts/templates/registration/login.html", context={"form": login_form})
+        return render(
+            request,
+            "Accounts/templates/registration/login.html",
+            context={"form": login_form},
+        )
 
 
 def register(request):
     if request.method == "POST":
         user_form = MyUserCreationForm(data=request.POST)
         if user_form.is_valid():
+            user_form.cleaned_data["is_active"] = False
             user = user_form.save()
             id = user.id
             endcode_id = encode_int_urlsafe(id)
             secret_code = secert_code_genarator()
             request.session["verify_code"] = secret_code
-            print(secret_code)
+            send_mail(
+                "verify_code",
+                f"Hello Here is your verification code {secret_code}",
+                "from@example.com",
+                [user.email],
+            )
             return redirect("verify_secret_code", user_id=endcode_id)
         else:
-            return render(request, "accounts/register.html", context={"form": user_form})
+            return render(
+                request, "accounts/register.html", context={"form": user_form}
+            )
     else:
         uesr_register_form = MyUserCreationForm()
-        return render(request, "accounts/register.html", context={"form": uesr_register_form})
+        return render(
+            request, "accounts/register.html", context={"form": uesr_register_form}
+        )
 
 
 @login_required
 def update_profile(request):
     if request.method == "POST":
-        user_data = MyUserChangeForm(instance=request.user, data=request.POST, files=request.FILES)
+        user_data = MyUserChangeForm(
+            instance=request.user, data=request.POST, files=request.FILES
+        )
         if user_data.is_valid():
             user_data.save()
             messages.success(request, "Profile updated successfully")
-            return redirect(reverse("visit_profile", kwargs={"username": request.user.username}))
+            return redirect(
+                reverse("visit_profile", kwargs={"username": request.user.username})
+            )
         else:
-            return render(request, "accounts/update_profile.html", context={"form": user_data})
+            return render(
+                request, "accounts/update_profile.html", context={"form": user_data}
+            )
     else:
         user_form = MyUserChangeForm(instance=request.user)
-        return render(request, "accounts/update_profile.html", context={"form": user_form})
+        return render(
+            request, "accounts/update_profile.html", context={"form": user_form}
+        )
 
 
 @login_required
@@ -73,7 +100,11 @@ def visit_profile(request, username):
             current_page_posts = paginator.page(1)
         except EmptyPage:
             raise Http404("The page Not Found")
-        return render(request, "accounts/profile.html", context={"posts": current_page_posts, "user": user})
+        return render(
+            request,
+            "accounts/profile.html",
+            context={"posts": current_page_posts, "user": user},
+        )
     raise Http404("The page Not Found")
 
 
@@ -90,14 +121,18 @@ def verify_secret_code(request, user_id):
                 user.is_active = True
                 user.save()
                 dj_login(request, user)
-                return redirect("visit_profile")
+                return redirect("visit_profile", username=user.username)
             else:
                 form_errors = {"invalid_code": "Code is Not Correct"}
         else:
             form_errors = code_form.errors
 
     form = SecertCodeForm()
-    return render(request, "accounts/secret_code.html", context={"form": form, "form_errors": form_errors})
+    return render(
+        request,
+        "accounts/secret_code.html",
+        context={"form": form, "form_errors": form_errors},
+    )
 
 
 @login_required
