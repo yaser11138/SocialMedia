be9832079eb8432d38b0b5b8ef5e10ee4c3a4a9a
yaser <ydahgan@gmail.com>
2024-02-09 17:11:16 +0330

    change is_active default to false

--- Accounts/models.py
+++ Accounts/models.py
@@ -5,27 +5,35 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 def user_directory_path(instance, filename):
-    return '{0}/profile_photo/{1}'.format(instance.username, filename)
+    return "{0}/profile_photo/{1}".format(instance.username, filename)
 
 
 class MyUser(AbstractUser):
     phone_number = PhoneNumberField(blank=True)
     email = models.EmailField(unique=True, validators=[EmailValidator])
-    photo = models.ImageField(upload_to=user_directory_path, default="default-profile.png")
+    photo = models.ImageField(
+        upload_to=user_directory_path, default="default-profile.png"
+    )
     is_active = models.BooleanField(
-        default=False,
+        default=True,
         help_text=(
             "Designates whether this user should be treated as active. "
             "Unselect this instead of deleting accounts."
         ),
     )
     bio = models.TextField(null=True, blank=True)
-    following = models.ManyToManyField(to="self", through="Contact", related_name="followers", symmetrical=False)
+    following = models.ManyToManyField(
+        to="self", through="Contact", related_name="followers", symmetrical=False
+    )
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "password"]
 
 
 class Contact(models.Model):
-    follower_user = models.ForeignKey(to=MyUser, related_name="contact_followers", on_delete=models.CASCADE)
-    following_user = models.ForeignKey(to=MyUser, related_name="contact_followings", on_delete=models.CASCADE)
+    follower_user = models.ForeignKey(
+        to=MyUser, related_name="contact_followers", on_delete=models.CASCADE
+    )
+    following_user = models.ForeignKey(
+        to=MyUser, related_name="contact_followings", on_delete=models.CASCADE
+    )
     date_followed = models.DateTimeField(auto_now_add=True)
